9282b5302c38ef67c1c21fd524e0dacc14f4a8cb
Keks <keks@htmlacademy.ru>
2023-04-11 15:28:27 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/likes.js
@@ -0,0 +1,16 @@
+let likeIcons = document.querySelectorAll('.like-rating__like-icon');
+let likeValues = document.querySelectorAll('.like-rating__value');
+
+for (let i = 0; i < likeIcons.length; i++) {
+    likeIcons[i].addEventListener('click', function () {
+      if (likeIcons[i].classList.contains('like-rating__button--not-click')) {
+        likeIcons[i].classList.remove('like-rating__button--not-click');
+        likeIcons[i].classList.add('like-rating__button--click');
+        likeValues[i].innerHTML = Number(likeValues[i].innerHTML) + 1;
+      } else {
+        likeIcons[i].classList.add('like-rating__button--not-click');
+        likeIcons[i].classList.remove('like-rating__button--click');
+        likeValues[i].innerHTML = Number(likeValues[i].innerHTML) - 1;
+      }
+    })
+  }
